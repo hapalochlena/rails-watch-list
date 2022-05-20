c2123c3bdddffd52cd52b2168f3d9ae1c6ee3a07
Lena Schneider <lena.schneider407@gmail.com>
2022-05-20 15:11:29 +0200

    rootpath + new bootstrap setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   #get '/lists', to: 'lists#index'
-  resources :lists
+  resources :lists, only: [:show, :new, :create]
 
 end

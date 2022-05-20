bb8140e7257760cc49c00f2e1abd1a12cc6fede9
Lena Schneider <lena.schneider407@gmail.com>
2022-05-20 14:56:43 +0200

    commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,18 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(params[:list])
+    list.save
+  end
+end
